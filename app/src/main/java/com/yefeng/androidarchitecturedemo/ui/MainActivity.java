da4618bd8041ae6b412edc65f04f46441a09b013
yefeng <yefeng.freedom@gmail.com>
2017-02-08 14:14:25 +0800

    delete some test code

--- app/src/main/java/com/yefeng/androidarchitecturedemo/ui/MainActivity.java
+++ app/src/main/java/com/yefeng/androidarchitecturedemo/ui/MainActivity.java
@@ -7,17 +7,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import com.yefeng.androidarchitecturedemo.R;
-import com.yefeng.androidarchitecturedemo.data.model.book.Book;
 import com.yefeng.androidarchitecturedemo.data.source.book.BookRepository;
 import com.yefeng.androidarchitecturedemo.data.source.book.local.BookLocalDataSource;
 import com.yefeng.androidarchitecturedemo.data.source.book.memory.BookMemoryDataSource;
 import com.yefeng.androidarchitecturedemo.data.source.book.remote.BookRemoteDataSource;
 import com.yefeng.support.http.HttpSchedulersTransformer;
-
-import org.reactivestreams.Subscriber;
-import org.reactivestreams.Subscription;
-
-import java.util.List;
 
 import timber.log.Timber;
 
@@ -55,55 +49,6 @@ public class MainActivity extends AppCompatActivity {
                 }, t -> Timber.e(t), () -> {
                     Timber.d("onComplete()");
                     mPd.dismiss();
-                });
-    }
-
-    private void testLocalData() {
-        new BookLocalDataSource().getBooks()
-                .compose(new HttpSchedulersTransformer<>())
-                .doOnSubscribe(subscription -> mPd.show())
-                .subscribe(books -> {
-                    Timber.e(books.toString());
-                    Timber.d("method: %s, thread: %s_%s", "testLocalData()", Thread.currentThread().getName(), Thread.currentThread().getId());
-                }, Timber::e, () -> mPd.dismiss());
-    }
-
-    private void testMemoryData() {
-        new BookMemoryDataSource().getBooks()
-                .compose(new HttpSchedulersTransformer<>())
-                .doOnSubscribe(subscription -> mPd.show())
-                .subscribe(books -> {
-                    Timber.e(books.toString());
-                    Timber.d("method: %s, thread: %s_%s", "testMemoryData()", Thread.currentThread().getName(), Thread.currentThread().getId());
-                }, Timber::e, () -> mPd.dismiss());
-    }
-
-    private void testRemoteData() {
-        new BookRemoteDataSource().getBooks()
-                .compose(new HttpSchedulersTransformer<>())
-                .doOnSubscribe(subscription -> mPd.show())
-                .subscribe(new Subscriber<List<Book>>() {
-                    @Override
-                    public void onSubscribe(Subscription s) {
-                        s.request(Long.MAX_VALUE);//设置请求数
-                    }
-
-                    @Override
-                    public void onNext(List<Book> books) {
-                        Timber.e(books.toString());
-                        Timber.d("method: %s, thread: %s_%s", "testRemoteData()", Thread.currentThread().getName(), Thread.currentThread().getId());
-                    }
-
-                    @Override
-                    public void onError(Throwable t) {
-                        mPd.dismiss();
-                        Timber.e(t);
-                    }
-
-                    @Override
-                    public void onComplete() {
-                        mPd.dismiss();
-                    }
                 });
     }
 }
