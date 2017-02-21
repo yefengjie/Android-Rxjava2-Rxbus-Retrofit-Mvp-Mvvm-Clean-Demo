3fbd974a8049bb3169c90f7ee8f8dab9318f220c
yefeng <yefeng.freedom@gmail.com>
2017-02-21 14:49:58 +0800

    add mvvm activity

---
+++ app/src/main/java/com/yefeng/androidarchitecturedemo/ui/mvvm/MvvmActivity.java
@@ -0,0 +1,10 @@
+package com.yefeng.androidarchitecturedemo.ui.mvvm;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by yefeng on 21/02/2017.
+ */
+
+public class MvvmActivity extends AppCompatActivity {
+}
