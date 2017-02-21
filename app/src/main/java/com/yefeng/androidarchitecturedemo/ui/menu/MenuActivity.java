0382af7d6f0617969e2b3a98aa19e4eccb3c06f0
yefeng <yefeng.freedom@gmail.com>
2017-02-21 12:05:20 +0800

    add menu

---
+++ app/src/main/java/com/yefeng/androidarchitecturedemo/ui/menu/MenuActivity.java
@@ -0,0 +1,94 @@
+package com.yefeng.androidarchitecturedemo.ui.menu;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.freedom.yefeng.yfrecyclerview.YfListInterface;
+import com.freedom.yefeng.yfrecyclerview.YfListRecyclerView;
+import com.yefeng.androidarchitecturedemo.R;
+import com.yefeng.androidarchitecturedemo.ui.mvp.MainActivity;
+import com.yefeng.support.base.AppInfo;
+
+import java.util.ArrayList;
+
+/**
+ * Created by yefeng on 21/02/2017.
+ */
+
+public class MenuActivity extends AppCompatActivity {
+    ImageView mIvShow;
+    Toolbar mTb;
+    CollapsingToolbarLayout mCtl;
+    CoordinatorLayout mCl;
+    YfListRecyclerView mList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        mIvShow = (ImageView) findViewById(R.id.iv_show);
+        mTb = (Toolbar) findViewById(R.id.tb);
+        mCtl = (CollapsingToolbarLayout) findViewById(R.id.ctl);
+        mCl = (CoordinatorLayout) findViewById(R.id.cl);
+        mList = (YfListRecyclerView) findViewById(R.id.rv);
+        initToolbar();
+        init();
+    }
+
+    private void initToolbar() {
+        setSupportActionBar(mTb);
+        final ActionBar ab = getSupportActionBar();
+        if (null != ab) {
+            ab.setHomeAsUpIndicator(R.drawable.ic_nav_story);
+            ab.setDisplayHomeAsUpEnabled(true);
+        }
+        mTb.setTitleTextColor(getResources().getColor(R.color.white));
+        mCtl.setTitle("Android Architecture Demo");
+        mCtl.setExpandedTitleColor(getResources().getColor(R.color.colorAccent));
+        int height = AppInfo.width / 4 * 3;
+        CollapsingToolbarLayout.LayoutParams lp = new CollapsingToolbarLayout.LayoutParams(AppInfo.width, height);
+        mIvShow.setLayoutParams(lp);
+    }
+
+    private void init() {
+        ArrayList<String> actions = new ArrayList<String>();
+        actions.add("Android Mvp Architecture Demo");
+        actions.add("Android Clean Architecture Demo");
+        actions.add("Android Mvvm Architecture Demo");
+        MenuAdapter adapter = new MenuAdapter(actions);
+        mList.setHasFixedSize(true);
+        mList.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        mList.setAdapter(adapter);
+        adapter.setOnItemClickListener(new YfListInterface.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, Object o) {
+                if ("Android Clean Architecture Demo".equals(o.toString())) {
+                    startActivity(new Intent(MenuActivity.this, MainActivity.class));
+                } else {
+                    startActivity(new Intent(MenuActivity.this, MainActivity.class));
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            Uri uri = Uri.parse("https://github.com/yefengfreedom");
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
