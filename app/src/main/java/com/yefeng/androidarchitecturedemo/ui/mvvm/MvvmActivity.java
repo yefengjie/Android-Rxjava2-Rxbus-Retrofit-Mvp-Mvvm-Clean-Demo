97d0356b2a7f6195536f5e3d68392aa09bd21b48
yefeng <yefeng.freedom@gmail.com>
2017-02-22 10:17:48 +0800

    add databinding

--- app/src/main/java/com/yefeng/androidarchitecturedemo/ui/mvvm/MvvmActivity.java
+++ app/src/main/java/com/yefeng/androidarchitecturedemo/ui/mvvm/MvvmActivity.java
@@ -1,10 +1,26 @@
 package com.yefeng.androidarchitecturedemo.ui.mvvm;
 
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import com.yefeng.androidarchitecturedemo.R;
+import com.yefeng.androidarchitecturedemo.data.model.book.Book;
+import com.yefeng.androidarchitecturedemo.databinding.ActivityMvvmBinding;
 
 /**
  * Created by yefeng on 21/02/2017.
  */
 
 public class MvvmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mvvm);
+        ActivityMvvmBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_mvvm);
+        Book book = new Book(1l, "book 1", "");
+        binding.setBook(book);
+    }
 }
