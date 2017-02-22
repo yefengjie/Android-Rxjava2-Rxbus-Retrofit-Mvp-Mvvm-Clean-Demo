0bffd57d42ccb95e44616038ecd2114a8f6c8877
yefeng <yefeng.freedom@gmail.com>
2017-02-22 11:19:53 +0800

    add rxbus

--- app/src/main/java/com/yefeng/androidarchitecturedemo/ui/mvvm/MvvmActivity.java
+++ app/src/main/java/com/yefeng/androidarchitecturedemo/ui/mvvm/MvvmActivity.java
@@ -2,32 +2,132 @@ package com.yefeng.androidarchitecturedemo.ui.mvvm;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.yefeng.androidarchitecturedemo.R;
 import com.yefeng.androidarchitecturedemo.data.model.book.Book;
+import com.yefeng.androidarchitecturedemo.data.source.book.BookRepository;
+import com.yefeng.androidarchitecturedemo.data.source.book.local.BookLocalDataSource;
+import com.yefeng.androidarchitecturedemo.data.source.book.memory.BookMemoryDataSource;
+import com.yefeng.androidarchitecturedemo.data.source.book.remote.BookRemoteDataSource;
 import com.yefeng.androidarchitecturedemo.databinding.ActivityMvvmBinding;
+import com.yefeng.androidarchitecturedemo.ui.mvp.MainContract;
+
+import java.util.ArrayList;
 
 /**
  * Created by yefeng on 21/02/2017.
  */
 
-public class MvvmActivity extends AppCompatActivity {
+public class MvvmActivity extends AppCompatActivity implements MainContract.View {
+
+    private SwipeRefreshLayout mSwipeLayout;
+    private MainContract.Presenter mPresenter;
+    private ActivityMvvmBinding mBinding;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mvvm);
-        ActivityMvvmBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_mvvm);
-        Book book = new Book(1l, "book 1", "");
-        binding.setBook(book);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_mvvm);
         init();
     }
 
     private void init() {
+
+        mPresenter = new MvvmPresenter(new BookRepository(new BookRemoteDataSource(), new BookLocalDataSource(), new BookMemoryDataSource()), this);
+        mBinding.setActionHandler(mPresenter);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mBinding.swipeContainer.setColorSchemeResources(
+                android.R.color.holo_blue_light,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
+        Book book = new Book(1l, "book 1", "");
+        mBinding.setBook(book);
+    }
+
+    private void showToast(String msg) {
+        Snackbar.make(mSwipeLayout, msg, Snackbar.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mPresenter.subscribe();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mPresenter.unSubscribe();
+    }
+
+    @Override
+    public void onLoading() {
+        if (!mBinding.swipeContainer.isRefreshing()) {
+            mBinding.swipeContainer.setRefreshing(true);
+        }
+    }
+
+    @Override
+    public void onLoadOk(ArrayList<Book> books) {
+
+    }
+
+    @Override
+    public void onLoadError(String msg) {
+        mBinding.swipeContainer.setRefreshing(false);
+        showToast(msg);
+    }
+
+    @Override
+    public void onLoadFinish() {
+        mBinding.swipeContainer.setRefreshing(false);
+    }
+
+    @Override
+    public void addBook(View view) {
+
+    }
+
+    @Override
+    public void deleteBook(@NonNull String id) {
+
+    }
+
+    @Override
+    public void onAction() {
+
+    }
+
+    @Override
+    public void onActionOk() {
+
+    }
+
+    @Override
+    public void onActionError(String msg) {
+
+    }
+
+    @Override
+    public void onActionFinish() {
+
+    }
+
+    @Override
+    public void setPresenter(MainContract.Presenter presenter) {
+
     }
 }
---
+++ app/src/main/java/com/yefeng/androidarchitecturedemo/ui/mvvm/MvvmPresenter.java
@@ -0,0 +1,111 @@
+package com.yefeng.androidarchitecturedemo.ui.mvvm;
+
+import android.support.annotation.NonNull;
+
+import com.yefeng.androidarchitecturedemo.data.model.book.Book;
+import com.yefeng.androidarchitecturedemo.data.source.book.BookRepository;
+import com.yefeng.androidarchitecturedemo.ui.mvp.Events;
+import com.yefeng.androidarchitecturedemo.ui.mvp.MainContract;
+import com.yefeng.support.http.HttpSchedulersTransformer;
+import com.yefeng.support.rxbus.RxBus;
+
+import org.reactivestreams.Subscription;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.functions.Action;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+import timber.log.Timber;
+
+/**
+ * Created by yefeng on 22/02/2017.
+ */
+
+public class MvvmPresenter implements MainContract.Presenter {
+
+    private final BookRepository mBookRepositoty;
+    private final MainContract.View mMvvnView;
+    private CompositeDisposable mCompositeDisposable;
+
+    public MvvmPresenter(BookRepository bookRepository, MainContract.View mvvmActivity) {
+        mBookRepositoty = bookRepository;
+        mMvvnView = mvvmActivity;
+    }
+
+    @Override
+    public void saveBook(@NonNull Book book) {
+
+    }
+
+    @Override
+    public void deleteBook(@NonNull String id) {
+
+    }
+
+    @Override
+    public void loadBooks(boolean forceUpdate) {
+        mBookRepositoty.getBooks(forceUpdate)
+                .compose(new HttpSchedulersTransformer<>())
+                .doOnSubscribe(new Consumer<Subscription>() {
+                    @Override
+                    public void accept(Subscription subscription) throws Exception {
+                        Timber.d("method: %s, thread: %s_%s", "doOnSubscribe()", Thread.currentThread().getName(), Thread.currentThread().getId());
+                        Timber.d("doOnSubscribe()");
+                        mMvvnView.onLoading();
+                    }
+                })
+                .subscribe(new Consumer<List<Book>>() {
+                    @Override
+                    public void accept(List<Book> books) throws Exception {
+                        Timber.d("onNext()");
+                        Timber.e(books.toString());
+                        Timber.d("method: %s, thread: %s_%s", "onNext()", Thread.currentThread().getName(), Thread.currentThread().getId());
+                        mMvvnView.onLoadOk(new ArrayList<>(books));
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Exception {
+                        Timber.d("method: %s, thread: %s_%s", "onError()", Thread.currentThread().getName(), Thread.currentThread().getId());
+                        Timber.e(throwable);
+                        mMvvnView.onLoadError(throwable.getMessage());
+                    }
+                }, new Action() {
+                    @Override
+                    public void run() throws Exception {
+                        Timber.d("method: %s, thread: %s_%s", "onComplete()", Thread.currentThread().getName(), Thread.currentThread().getId());
+                        Timber.d("onComplete()");
+                        mMvvnView.onLoadFinish();
+                    }
+                });
+    }
+
+    @Override
+    public void subscribe() {
+        initRxBus();
+        loadBooks(false);
+    }
+
+    private void initRxBus() {
+        if (null == mCompositeDisposable) {
+            mCompositeDisposable = new CompositeDisposable();
+        }
+        mCompositeDisposable.add(RxBus.getBus()
+                .toObserverable()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(o -> {
+                    if (o instanceof Events.ReloadEvent) {
+                        loadBooks(((Events.ReloadEvent) o).mForceUpdate);
+                    }
+                }));
+    }
+
+    @Override
+    public void unSubscribe() {
+
+    }
+}
